244b77c25ef3b7868e4b99a9c99c870eec93b432
Jan Piotrowski <piotrowski+github@gmail.com>
2018-09-20 16:59:26 +0200

    add apache/cordova as valid documentation repo

--- include/repositories.php
+++ include/repositories.php
@@ -3,85 +3,86 @@ $repositories = [
 	'Platforms' => [
 		'Active Platforms' => [
 			'apache/cordova-android',
-    		'apache/cordova-ios',
-    		'apache/cordova-windows',
-    		'apache/cordova-browser',
-    		'apache/cordova-osx',
-    		'apache/cordova-test-platform'
+			'apache/cordova-ios',
+			'apache/cordova-windows',
+			'apache/cordova-browser',
+			'apache/cordova-osx',
+			'apache/cordova-test-platform'
 		],
 		'Experimental or Unfinished Platforms' => [
 			'apache/cordova-electron'
 		],
 		'Deprecated platforms' => [
 			'apache/cordova-blackberry',
-    		'apache/cordova-firefoxos',
-    		'apache/cordova-ubuntu',
-    		'apache/cordova-wp8',
-    		'apache/cordova-tizen',
-    		'apache/cordova-qt',
-    		'apache/cordova-webos',
-    		'apache/cordova-amazon-fireos',
-    		'apache/cordova-wp7',
-    		'apache/cordova-bada',
-    		'apache/cordova-bada-wac'
-		]	
+			'apache/cordova-firefoxos',
+			'apache/cordova-ubuntu',
+			'apache/cordova-wp8',
+			'apache/cordova-tizen',
+			'apache/cordova-qt',
+			'apache/cordova-webos',
+			'apache/cordova-amazon-fireos',
+			'apache/cordova-wp7',
+			'apache/cordova-bada',
+			'apache/cordova-bada-wac'
+		]
 	],
 	'Core Plugins' => [
 		'Active Core Plugins' => [
 			'apache/cordova-plugin-battery-status',
-    		'apache/cordova-plugin-camera',
-    		'apache/cordova-plugin-device',
-    		'apache/cordova-plugin-dialogs',
-    		'apache/cordova-plugin-file',
-    		'apache/cordova-plugin-geolocation',
-    		'apache/cordova-plugin-inappbrowser',
-    		'apache/cordova-plugin-media',
-    		'apache/cordova-plugin-media-capture',
-    		'apache/cordova-plugin-network-information',
-    		'apache/cordova-plugin-screen-orientation',
-    		'apache/cordova-plugin-splashscreen',
-    		'apache/cordova-plugin-statusbar',
-    		'apache/cordova-plugin-vibration',
-    		'apache/cordova-plugin-whitelist',
-    		'apache/cordova-plugin-wkwebview-engine',
+			'apache/cordova-plugin-camera',
+			'apache/cordova-plugin-device',
+			'apache/cordova-plugin-dialogs',
+			'apache/cordova-plugin-file',
+			'apache/cordova-plugin-geolocation',
+			'apache/cordova-plugin-inappbrowser',
+			'apache/cordova-plugin-media',
+			'apache/cordova-plugin-media-capture',
+			'apache/cordova-plugin-network-information',
+			'apache/cordova-plugin-screen-orientation',
+			'apache/cordova-plugin-splashscreen',
+			'apache/cordova-plugin-statusbar',
+			'apache/cordova-plugin-vibration',
+			'apache/cordova-plugin-whitelist',
+			'apache/cordova-plugin-wkwebview-engine',
 		],
 		'Experimental or Unfinished Core Plugins' => [
 			'apache/cordova-plugins'
 		],
 		'Deprecated Core Plugins' => [
 			'apache/cordova-plugin-console',
-    		'apache/cordova-plugin-contacts',
-    		'apache/cordova-plugin-device-motion',
-    		'apache/cordova-plugin-device-orientation',
-    		'apache/cordova-plugin-file-transfer',
-    		'apache/cordova-plugin-globalization',
-    		'apache/cordova-plugin-legacy-whitelist',
-		]	
+			'apache/cordova-plugin-contacts',
+			'apache/cordova-plugin-device-motion',
+			'apache/cordova-plugin-device-orientation',
+			'apache/cordova-plugin-file-transfer',
+			'apache/cordova-plugin-globalization',
+			'apache/cordova-plugin-legacy-whitelist',
+		]
 	],
 	'Tooling' => [
 		'CLIs' => [
 			'apache/cordova-cli',
-    		'apache/cordova-plugman',
-    		'apache/cordova-coho'
+			'apache/cordova-plugman',
+			'apache/cordova-coho'
 		],
 		'Libraries' => [
 			'apache/cordova-js',
-    		'apache/cordova-lib',
-    		'apache/cordova-common',
-    		'apache/cordova-create',
-    		'apache/cordova-fetch',
-    		'apache/cordova-serve'
+			'apache/cordova-lib',
+			'apache/cordova-common',
+			'apache/cordova-create',
+			'apache/cordova-fetch',
+			'apache/cordova-serve'
 		],
 		'Testing' => [
 			'apache/cordova-plugin-test-framework',
-    		'apache/cordova-paramedic',
-    		'apache/cordova-mobile-spec'
-		]	
+			'apache/cordova-paramedic',
+			'apache/cordova-mobile-spec'
+		]
 	],
 	'Templates' => [
 		'apache/cordova-app-hello-world'
 	],
 	'Documentation and Website(s)' => [
+		'apache/cordova',
 		'apache/cordova-docs',
 		'apache/cordova-status'
 	],
