f6ad2684866c94d2880b0fddcfc2492bc4cba30e
Jan Piotrowski <piotrowski+github@gmail.com>
2018-09-28 21:22:51 +0200

    additional markdown list of repos with deprecated ones behind <details>

--- cordova-github-repositories_markdown.php
+++ cordova-github-repositories_markdown.php
@@ -4,7 +4,7 @@ include("include/repositories.php");
 
 <h1>Grouped list of Cordova repositories on Github as Markdown</h1>
 
-<textarea style="width:100%; height:80%;">
+<textarea style="width:100%; height:50%;">
 <!-- This markdown was generated with http://cordova.betamo.de/cordova-github-repositories_markdown.php -->
 
 <?php
@@ -21,16 +21,57 @@ foreach($repositories as $group => $categories)
 	echo "### ".$group."\n";
 
 	if(isset($categories[0])) {
-		$repositories = $categories;
-		output_repositories($repositories);
+		$_repositories = $categories;
+		output_repositories($_repositories);
 	}
 	else
 	{
-		foreach($categories as $category => $repositories) 
+		foreach($categories as $category => $_repositories) 
 		{
 			echo "#### ".$category."\n";
-			output_repositories($repositories);
+			output_repositories($_repositories);
 		}
+	}
+}
+?>
+</textarea>
+
+Deprecated lists hidden behind &lt;details&gt;:
+<textarea style="width:100%; height:50%;">
+<!-- This markdown was generated with http://cordova.betamo.de/cordova-github-repositories_markdown.php -->
+
+<?php
+foreach($repositories as $group => $categories) 
+{
+	if(str_replace("Deprecated", "", $group) != $group || str_replace("Organization", "", $group) != $group) {
+		echo "<details><summary>$group</summary>\n\n";
+	}
+
+	echo "### ".$group."\n";
+
+	if(isset($categories[0])) {
+		$_repositories = $categories;
+		output_repositories($_repositories);
+	}
+	else
+	{
+		foreach($categories as $category => $_repositories) 
+		{
+			if(str_replace("Deprecated", "", $category) != $category) {
+				echo "<details><summary>$category</summary>\n\n";
+			}
+
+			echo "#### ".$category."\n";
+			output_repositories($_repositories);
+
+			if(str_replace("Deprecated", "", $category) != $category) {
+				echo "</details>\n\n";
+			}
+		}
+	}
+
+	if(str_replace("Deprecated", "", $group) != $group || str_replace("Organization", "", $group) != $group) {
+		echo "</details>\n\n";
 	}
 }
 ?>
