6cdca374876256f043a2d170364a971784bd0224
Jan Piotrowski <piotrowski+git@gmail.com>
2019-05-29 13:31:47 +0200

    checkbox list with linked repo names

--- cordova-github-repositories-checkboxes_markdown.php
+++ cordova-github-repositories-checkboxes_markdown.php
@@ -45,3 +45,47 @@ foreach($repositories as $group => $categories)
 }
 ?>
 </textarea>
+
+Same with links to the repos:
+<textarea style="width:100%; height:50%;">
+<!-- This markdown was generated with http://cordova.betamo.de/cordova-github-repositories-checkboxes_markdown.php -->
+
+<?php
+function output_repositories2($repositories) {
+	#echo "\n";
+	foreach($repositories as $repository) {
+		$repo = str_replace("apache/", "", $repository);
+		echo "- [ ] [".$repo."](https://github.com/".$repository.")\n";
+	}
+	#echo "\n";
+}
+
+foreach($repositories as $group => $categories) 
+{	
+	if(str_replace("Deprecated", "", $group) != $group || str_replace("Organization", "", $group) != $group) {
+		continue;
+	}
+
+	echo "#### ".$group."\n";
+
+
+	if(isset($categories[0])) {
+		$_repositories = $categories;
+		output_repositories2($_repositories);
+	}
+	else
+	{
+		foreach($categories as $category => $_repositories) 
+		{
+			if(str_replace("Deprecated", "", $category) != $category) {
+				continue;
+			}
+			
+			echo "##### ".$category."\n";
+			output_repositories2($_repositories);
+		}
+	}
+	echo "\n";
+}
+?>
+</textarea>
