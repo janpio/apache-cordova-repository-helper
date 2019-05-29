05cfafa9fca7eaa5982de38204fe7ce96009e2ac
Jan Piotrowski <piotrowski+git@gmail.com>
2019-05-29 13:31:31 +0200

    alternative output with deprecated repositories hidden behind <details>

--- cordova-github-repositories_markdown.php
+++ cordova-github-repositories_markdown.php
@@ -35,3 +35,44 @@ foreach($repositories as $group => $categories)
 }
 ?>
 </textarea>
+
+Deprecated lists hidden behind &lt;details&gt;:
+<textarea style="width:100%; height:50%;">
+<!-- This markdown was generated with http://cordova.betamo.de/cordova-github-repositories_markdown.php -->
+
+<?php
+foreach($repositories as $group => $categories) 
+{
+	if(str_replace("Deprecated", "", $group) != $group || str_replace("Organization", "", $group) != $group) {
+		echo "<details><summary>$group</summary>\n\n";
+	}
+
+	echo "### ".$group."\n";
+
+	if(isset($categories[0])) {
+		$_repositories = $categories;
+		output_repositories($_repositories);
+	}
+	else
+	{
+		foreach($categories as $category => $_repositories) 
+		{
+			if(str_replace("Deprecated", "", $category) != $category) {
+				echo "<details><summary>$category</summary>\n\n";
+			}
+
+			echo "#### ".$category."\n";
+			output_repositories($_repositories);
+
+			if(str_replace("Deprecated", "", $category) != $category) {
+				echo "</details>\n\n";
+			}
+		}
+	}
+
+	if(str_replace("Deprecated", "", $group) != $group || str_replace("Organization", "", $group) != $group) {
+		echo "</details>\n\n";
+	}
+}
+?>
+</textarea>
